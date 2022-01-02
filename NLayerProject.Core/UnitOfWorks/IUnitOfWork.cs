6ffb97fba63e28699a544ab42219500140288053
oguzhantomak <oguzhantomak@gmail.com>
2022-01-02 18:53:28 +0300

    IUnitOfWork added to core layer and dbcontext added to data layer

---
+++ NLayerProject.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLayerProject.Core.Repositories;
+
+namespace NLayerProject.Core.UnitOfWorks
+{
+    internal interface IUnitOfWork
+    {
+        IProductRepository Products { get; }
+
+        ICategoryRepository Categories { get; }
+
+        /// <summary>
+        /// CommitAsync, implement ettiğimiz zaman SaveChanges metodunu çağıracak.
+        /// </summary>
+        /// <returns></returns>
+        Task CommitAsync();
+
+        void Commit();
+    }
+}
---
+++ NLayerProject.Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NLayerProject.Core.Models;
+
+namespace NLayerProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
